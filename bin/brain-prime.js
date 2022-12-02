3337421fbfd8ceafee6c9c956f97aaab1c41b0e8
Goga-Rid <wopa22@list.ru>
2022-12-02 18:42:39 +0300

    make brain-prime game

---
+++ bin/brain-prime.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import primeGame from '../src/games/g-prime.js';
+
+primeGame();
---
+++ src/games/g-prime.js
@@ -0,0 +1,29 @@
+import startGame from "../index.js";
+import genRandNum from "../genRandNum.js";
+
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const primeLogic = (num) => {
+  if (num < 2) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(num); i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+const rulQuestAndAns = () => {
+    const question = genRandNum(1,30);
+    const answer = primeLogic(question) ? 'yes' : 'no';
+    return [question, answer];
+}
+
+const primeGame = () => {
+    startGame(description, rulQuestAndAns);
+}
+
+export default primeGame;
