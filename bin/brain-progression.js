fc16a142364a5a12866f20f9f13ce47ce45e7c8b
Goga-Rid <wopa22@list.ru>
2022-12-02 17:09:48 +0300

    make brain-progression game

---
+++ bin/brain-progression.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import progressGame from '../src/games/g-progression.js';
+
+progressGame();
---
+++ src/games/g-progression.js
@@ -0,0 +1,31 @@
+import startGame from "../index.js";
+import genRandNum from "../genRandNum.js";
+
+const description = "What number is missing in the progression?";
+
+const progressLogic = (progressBegin, progressLength, stepProgress) => {
+    let arr = [];
+    for (let i = 0; i < progressLength; i +=1){
+// ------ progression formation. ---------------
+        arr.push(progressBegin + (i * stepProgress));
+    }
+    const pass = arr[Math.floor(Math.random() * arr.length)];
+    arr[arr.indexOf(pass)] = '..';
+    arr = String(arr.join(' '));
+    return [arr, pass];
+}
+
+const rulQuestAndAns = () => {
+    const progressBegin = genRandNum(1,50);
+    const progressLength = genRandNum(5,10);
+    const passNum = genRandNum(1,10);
+    const [question, currAnswer] = progressLogic(progressBegin, progressLength, passNum);
+    const answer = String(currAnswer);
+    return [question, answer];
+}
+
+const progressGame = () => {
+    startGame(description, rulQuestAndAns);
+}
+
+export default progressGame;
